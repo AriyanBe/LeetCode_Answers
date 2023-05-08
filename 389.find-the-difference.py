e37461163dfee72ba48931d3e1976b889b0db233
Ariyan B <95265960+AriyanBe@users.noreply.github.com>
2023-05-07 19:47:50 -0500

    Add files via upload

---
+++ 389.find-the-difference.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=389 lang=python
+#
+# [389] Find the Difference
+#
+
+# @lc code=start
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
+# @lc code=end
+
